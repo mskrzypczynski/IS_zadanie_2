9ae27cafdab9ca96229040e7f13e6d2f69f59c8c
mskrzypczynski <mikolaj.skrzypczynski@gmail.com>
2022-11-04 21:21:25 +0100

    m

--- src/main/java/org/example/LaptopsBean.java
+++ src/main/java/org/example/LaptopsBean.java
@@ -1,20 +1,26 @@
 package org.example;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
-@WebService(endpointInterface = "LaptopsWebServiceInterface")
-public class LaptopsBean implements LaptopsInterface {
-    @Override
-    public int getManufacturerLaptopNumber(String manufacturer) {
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public class LaptopsBean {
+
+@WebMethod
+public int getManufacturerLaptopNumber(String manufacturer) {
         return 0;
-    }
 
-    @Override
+}
+
+@WebMethod
     public int getResolutionLaptopNumber(String resolution) {
         return 0;
     }
 
-    @Override
+    @WebMethod
     public Laptop[] getLaptopListByFeatures(String f1, String f2, String f3, String f4, String f5) {
         return null;
     }
